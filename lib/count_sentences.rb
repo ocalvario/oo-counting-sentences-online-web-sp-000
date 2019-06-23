da77361571cd91faed8cf2b6fda61ebf7097786c
Oswald Calvario <oswald.calvario@gmail.com>
2019-06-23 01:53:56 +0000

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -2,24 +2,19 @@ require 'pry'
 
 class String
 
-  def sentence?(string)
-   if  string.end_with?(".")
-     return true 
-      else 
-        return false 
-      end
+  def sentence?
+    self.end_with?(".")
   end
-end
 
   def question?
-
+    self.end_with?("?")
   end
 
   def exclamation?
-
+    self.end_with?("!")
   end
 
   def count_sentences
-
+    self.split(/\.|\?|\!/).delete_if {|w| w.size < 2}.size
   end
 end
